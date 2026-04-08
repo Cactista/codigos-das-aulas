d14f3042e1502c5cdc4776dd9c9dbc05062c9cc4
Cactista <matheus.alencar0512@gmail.com>
2026-04-08 15:34:44 -0300

    Add files via upload

---
+++ laços.py
@@ -0,0 +1,7 @@
+while True:
+    opcao = input('digite 0 para sair ou 1 para continuar')
+    if opcao == '0':
+        print("Saindo . . .")
+        break
+    else:
+        print('continuando o loop!')
